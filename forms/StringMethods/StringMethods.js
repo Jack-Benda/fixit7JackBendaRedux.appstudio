fe7242c9b6870840884677a8057efb052b4ca8db
Jack-Benda <55208631+Jack-Benda@users.noreply.github.com>
2019-09-29 22:20:47 -0500

    Initial commit

---
+++ forms/StringMethods/StringMethods.js
@@ -0,0 +1,19 @@
+let quoteString = "Failure is simply the opportunity to begin again this time more intelligently"
+let upperCaseString = quoteString.toUpperCase()
+alert("Upper case string is:" + quoteString)
+
+let authorString = "-Henry Ford"
+let completeString = quoteString.concat(authorString)
+
+let secondQuote = "Talent is cheaper than table salt. What seperates the talented individual from the successful one is a lot fo hard work. -Stephen King";
+lowerCaseString = secondQuote.toLowerCase();
+
+alert("Lowercase string is " + lowerCaseString)
+alert(`character at the 3rd position of secondQuote is` + secondQuote.charAt(3)))
+
+
+findString = secondQuote.slice(34)
+alert(findString)
+
+console.log(findString)
+//question on the word file
